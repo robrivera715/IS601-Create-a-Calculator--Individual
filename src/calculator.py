d6c8f4ce815ad22a0e91166f8cbc48ffffa33504
Roberto Rivera <rr679@njit.edu>
2019-10-23 12:42:25 -0400

    FEATURE:Added src directory with calculator.py python code and calculatortest.py folders

---
+++ src/calculator.py
@@ -0,0 +1,16 @@
+import math
+
+while 1 == 1:
+    print("Enter 'add' to add two number")
+    print("Enter 'mul' to multiply two number")
+    print("Enter 'div' to divide two number")
+    print("Enter 'sub' to subtract two number")
+    print("Enter 's' to get the square root of a number")
+    print("Enter 'quit' to end the program")
+    number = input(" ")
+
+    if number == "add":
+        num1 = int(input("Enter a valid number"))
+        num2 = int(input("Enter another number"))
+        total1 = str(num1 + num2)
+        print('The total number is ' + total)
