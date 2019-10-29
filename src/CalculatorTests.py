8879e9a2924048016561ac403be5d1703ef841e0
Roberto Rivera <rr679@njit.edu>
2019-10-29 16:01:22 -0400

    FEATURE: Updated Dockerfile, Calculator.py file, CalculatorTests.py file

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,48 +1,42 @@
 import unittest
 from Calculator import Calculator
+from CsvReader import CsvReader
+
 
 class MyTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
-        calculator = Calculator()
+        self.calculator = Calculator()
 
     def test_instantiate_calculator(self):
-        calculator = Calculator()
-        self.assertIsInstance(calculator, Calculator)
+        self.assertIsInstance(self.calculator, Calculator)
 
     def test_results_property_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.result, 0)
+        self.assertEqual(self.calculator.result, 0)
 
     def test_add_method_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.add(2, 2), 4)
-        self.assertEqual(calculator.result, 4)
+        self.assertEqual(self.calculator.add(2, 2), 4)
+        self.assertEqual(self.calculator.result, 4)
 
     def test_subtract_method_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.subtract(2, 2), 0)
-        self.assertEqual(calculator.result, 0)
+        self.assertEqual(self.calculator.subtract(2, 2), 0)
+        self.assertEqual(self.calculator.result, 0)
 
     def test_multiply_method_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.multiply(2, 2), 4)
-        self.assertEqual(calculator.result, 4)
+        self.assertEqual(self.calculator.multiply(2, 2), 4)
+        self.assertEqual(self.calculator.result, 4)
 
     def test_divide_method_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.divide(2, 2), 1)
-        self.assertEqual(calculator.result, 1)
+        self.assertEqual(self.calculator.divide(2, 2), 1)
+        self.assertEqual(self.calculator.result, 1)
 
     def test_square_method_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.square(2), 4)
-        self.assertEqual(calculator.result, 4)
+        self.assertEqual(self.calculator.square(2), 4)
+        self.assertEqual(self.calculator.result, 4)
 
     def test_square_root_method_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.squareroot(4), 2)
-        self.assertEqual(calculator.result, 2)
+        self.assertEqual(self.calculator.squareroot(4), 2)
+        self.assertEqual(self.calculator.result, 2)
 
 
 if __name__ == '__main__':
